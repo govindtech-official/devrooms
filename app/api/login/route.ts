6fb36ffda8020ce13c3594881e53bce4ddb99a0b
govindtech-official <contactgovindtech@gmail.com>
2026-02-28 14:02:13 +0530

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,20 +1,20 @@
+import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import User from "@/models/User";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     await connectDB();
 
-    const { username, password } = await req.json();
+    const { email, password } = await req.json();
 
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ email });
 
     if (!user) {
       return NextResponse.json(
-        { message: "Invalid credentials" },
+        { message: "User not found" },
         { status: 400 }
       );
     }
@@ -29,25 +29,18 @@ export async function POST(req: Request) {
     }
 
     const token = jwt.sign(
-      { id: user._id },
+      { id: user._id, name: user.name, email: user.email },
       process.env.JWT_SECRET!,
-      { expiresIn: "1d" }
+      { expiresIn: "7d" }
     );
 
-    const response = NextResponse.json({
-      message: "Login successful",
-    });
+    return NextResponse.json(
+      { message: "Login successful", token, user },
+      { status: 200 }
+    );
 
-    response.cookies.set("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "strict",
-      path: "/",
-    });
-
-    return response;
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return NextResponse.json(
       { message: "Server error" },
       { status: 500 }
